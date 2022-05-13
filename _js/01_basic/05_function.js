b61ea2031b05b1d4d3670999c0ab7064fa6d12a5
sungyeol702 <tjdfuf802@naver.com>
2022-05-13 22:41:10 +0900

    js_basic
    04_oper, 05_function

---
+++ _js/01_basic/05_function.js
@@ -0,0 +1,8 @@
+console.log("---------------------------")
+console.log("function.js에서 실행되는 코드")
+console.log("---------------------------")
+
+
+function outerFunc() {
+    console.log('function.js 에서 선언된 function')
+}
---
+++ _js/01_basic/05_function2.js
@@ -0,0 +1,27 @@
+function myFunc1() {
+    console.log(`너무 선언식이 많으면 따로 js파일에 모아둔다`)
+}
+function myFunc2() {
+    console.log(`너무 선언식이 많으면 따로 js파일에 모아둔다`)
+}
+function myFunc3() {
+    console.log(`너무 선언식이 많으면 따로 js파일에 모아둔다`)
+}
+function myFunc4() {
+    console.log(`너무 선언식이 많으면 따로 js파일에 모아둔다`)
+}
+function myFunc5() {
+    console.log(`너무 선언식이 많으면 따로 js파일에 모아둔다`)
+}
+function myFunc6() {
+    console.log(`너무 선언식이 많으면 따로 js파일에 모아둔다`)
+}
+function myFunc7() {
+    console.log(`너무 선언식이 많으면 따로 js파일에 모아둔다`)
+}
+function myFunc8() {
+    console.log(`너무 선언식이 많으면 따로 js파일에 모아둔다`)
+}
+function myFunc9() {
+    console.log(`너무 선언식이 많으면 따로 js파일에 모아둔다`)
+}
